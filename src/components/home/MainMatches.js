930f9f10d58eea1bac0129ce4f043ed03c6eaa5b
Melody Stern <melliemuse@gmail.com>
2020-03-17 22:22:20 -0500

    fleshed out main match display component

--- src/components/home/MainMatches.js
+++ src/components/home/MainMatches.js
@@ -5,7 +5,23 @@ export default class MainMatches extends Component {
     render() {
         return (
             <>
+            <img src={this.props.match.profile_pic}></img>
             <h3>{this.props.match.user.username}</h3>
+            <h4>Age</h4>
+            <p>{this.props.match.age}</p>
+            {this.props.match.smoker && <p>Smoker</p>}
+            {this.props.match.kids && <p>Has kids</p>}
+            <h4>Lives in </h4>
+            <p>{this.props.match.location}</p>
+            <h4>Looking for</h4>
+            <p> {this.props.match.looking_for}</p>
+            <h4>Interests include </h4>
+            <p>{this.props.match.interests}</p>
+            <h4>Tagline</h4>
+            <p>"{this.props.match.tagline}"</p>
+            <h4>Bio</h4>
+            <p>{this.props.match.bio}</p>
+            
             </>
         )
     }

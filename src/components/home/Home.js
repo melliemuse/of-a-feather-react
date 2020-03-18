d4b2c443fbca0e8a5609b3a9bf77e09b84d2aac8
Melody Stern <melliemuse@gmail.com>
2020-03-17 19:37:24 -0500

    wrote logic to filter matches by current user's attachment style id

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,11 +4,29 @@ import APIManager from '../helpers/APIManager'
 
 export default class Home extends Component {
     state = {
-        test: 'test'
+        attachment_style: null,
+        filter_id: 0,
+        matches: {}
     }
 
     componentDidMount() {
-        
+        APIManager.getAll("daters")
+        .then(response => {
+            const attachment_style = response[0].attachment_style_id
+            if (attachment_style !== 1) {
+                this.setState({filter_id: 1})
+            } 
+            else {
+                this.setState({filter_id: `1&2&3`})
+            }
+            this.setState({attachment_style: attachment_style})
+        })
+        .then(() => {
+            if (this.state.filter_id !== null) {
+                APIManager.getAll(`daters?attachment_style_id=${this.state.filter_id}`)
+                .then(response => this.setState({matches: response}))
+            }
+        })
     }
 
     render() {

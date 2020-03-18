6103b7853ce4f4f24e412cf4351981ab2859f0cc
Melody Stern <melliemuse@gmail.com>
2020-03-18 12:29:07 -0500

    wrote functions to handle accepting and rejecting matches

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,10 +8,13 @@ export default class Home extends Component {
     state = {
         attachment_style: null,
         filter_id: 0,
-        matches: []
+        dater_id: null,
+        matches: [],
+        match_status: []
     }
 
     componentDidMount() {
+        // Add exclude to exclude rejected and matched matches
         APIManager.getAll("daters")
             .then(response => {
                 const attachment_style = response[0].attachment_style_id
@@ -21,7 +24,10 @@ export default class Home extends Component {
                 else {
                     this.setState({ filter_id: `1&2&3` })
                 }
-                this.setState({ attachment_style: attachment_style })
+                this.setState({
+                    attachment_style: attachment_style,
+                    dater_id: response[0].id
+                })
             })
             .then(() => {
                 if (this.state.filter_id !== null) {
@@ -29,6 +35,73 @@ export default class Home extends Component {
                         .then(response => this.setState({ matches: response }))
                 }
             })
+            .then(() => {
+                APIManager.getAll("matchstatuses")
+                .then(response => this.setState({match_status: response}))
+            })
+    }
+
+    handleMatch = (matched_with_id) => {
+        
+        //    check if match exists for both users. 
+        APIManager.getAll(`matches?dater_id=${this.state.dater_id}&matched_with_id=${matched_with_id}`)
+            .then(response => {
+                console.log("response",response)
+                //create object for match table 
+                let match = {
+                    dater_id: this.state.dater_id,
+                    matched_with_id: matched_with_id
+                }
+                //    if so, change status to matched.
+                if (response[0]) {
+                    let match_status_id = this.state.match_status[1].id
+                    match.match_status_id = match_status_id
+                    match.id = response[0].id
+                    console.log("match exists", match)
+                    APIManager.update(`matches`, match)
+                    // .then(response => console.log(response))
+                }
+                //    if not, create it and change status to pending. 
+                else {
+                    match.match_status_id = this.state.match_status[0].id
+                    console.log("match doesn't exist", match)
+                    APIManager.createNew(`matches`, match)
+                    // .then(response => console.log(response))
+                }
+            })
+
+
+    }
+
+    handlePass = (matched_with_id) => {
+        //create object for match table 
+        let match = {
+            dater_id: this.state.dater_id,
+            matched_with_id: matched_with_id,
+            match_status_id: this.state.match_status[2].id
+        }
+        console.log(match)
+        // check if match exists for both users. 
+        // http://localhost:8000/matches?dater_id=${dater_id}&matched_with_id=${matched_with_id}
+        //    check if match exists for both users. 
+        APIManager.getAll(`matches?dater_id=${this.state.dater_id}&matched_with_id=${matched_with_id}`)
+            .then(response => {
+                console.log("response",response)
+                //    if so, change status to matched.
+                if (response[0]) {
+                    match.id = response[0].id
+                    console.log("match exists", match)
+                    APIManager.update(`matches`, match)
+                    // .then(response => console.log(response))
+                }
+                // if not, create it and change status to rejected. if so, change status to rejected.  
+                else {
+                    console.log("match doesn't exist", match)
+                    APIManager.createNew(`matches`, match)
+                    // .then(response => console.log(response))
+                }
+            })
+
     }
 
     render() {
@@ -42,6 +115,8 @@ export default class Home extends Component {
                 <div>
                     {this.state.matches.map(match =>
                         <MainMatches
+                            handleMatch={this.handleMatch}
+                            handlePass={this.handlePass}
                             match={match}
                             key={match.id}
                         />
--- src/components/home/MainMatches.js
+++ src/components/home/MainMatches.js
@@ -21,7 +21,14 @@ export default class MainMatches extends Component {
             <p>"{this.props.match.tagline}"</p>
             <h4>Bio</h4>
             <p>{this.props.match.bio}</p>
-            
+            <button 
+            // id={this.props.match.id}
+            onClick={() =>this.props.handleMatch(this.props.match.id)}
+            >Match</button>
+            <button 
+            // id={this.props.match.id}
+            onClick={() =>this.props.handlePass(this.props.match.id)}
+            >Pass</button>
             </>
         )
     }

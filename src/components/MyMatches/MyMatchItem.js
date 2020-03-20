202a6c5cb2bf0089c53e98ab1251d501a3038432
Melody Stern <melliemuse@gmail.com>
2020-03-19 21:32:52 -0500

    adjusted logic so that the dater profile displayed is of the user matched with the current logged in user

--- src/components/MyMatches/MyMatchItem.js
+++ src/components/MyMatches/MyMatchItem.js
@@ -4,24 +4,24 @@ export default class MyMatchItem extends Component {
     render() {
         return (
             <div className="main flex">
-                <img src={this.props.match.dater.profile_pic}></img>
-                <h3>{this.props.match.dater.user.first_name}</h3>
-                {/* <h4>Age</h4> */}
-                <p>{this.props.match.dater.age}</p>
-                {this.props.match.dater.smoker && <p>Smoker</p>}
-                {this.props.match.dater.kids && <p>Has kids</p>}
+                <img src={this.props.match.profile_pic}></img>
+                <h3>{this.props.match.user.first_name}</h3>
+                <h4>Age</h4>
+                <p>{this.props.match.age}</p>
+                {this.props.match.smoker && <p>Smoker</p>}
+                {this.props.match.kids && <p>Has kids</p>}
                 <h4>Lives in </h4>
-                <p>{this.props.match.dater.location}</p>
+                <p>{this.props.match.location}</p>
                 <h4>Looking for</h4>
-                <p> {this.props.match.dater.looking_for}</p>
+                <p> {this.props.match.looking_for}</p>
                 <h4>Interests include </h4>
-                <p>{this.props.match.dater.interests}</p>
+                <p>{this.props.match.interests}</p>
                 <h4>Tagline</h4>
-                <p>"{this.props.match.dater.tagline}"</p>
+                <p>"{this.props.match.tagline}"</p>
                 <h4>Bio</h4>
-                <p>{this.props.match.dater.bio}</p>
+                <p>{this.props.match.bio}</p>
                 <button>Message</button>
-                <button>Unmatch</button>
+                <button onClick={() => this.props.handleUnmatch(this.props.match)}>Unmatch</button>
             </div>
         )
 
--- src/components/MyMatches/MyMatchList.js
+++ src/components/MyMatches/MyMatchList.js
@@ -4,20 +4,60 @@ import MyMatchItem from './MyMatchItem'
 
 export default class MyMatchList extends Component {
     state = {
-        matches: []
+        matches: [],
+        currentUser: null
     }
 
     componentDidMount = () => {
-        APIManager.getAll(`matches?match_status_id=2`)
-            .then(response => this.setState({ matches: response }))
+        APIManager.getAll(`daters`)
+            .then(currentDater => {
+                this.setState({ currentUser: currentDater[0] })
+                APIManager.getAll(`matches?match_status_id=2`)
+                    .then(response => {
+                        // this.setState({ matches: response })
+                        console.log(response)
+                        console.log(currentDater)
+                        let matches = []
+                        response.map(match => {
+                            console.log(match)
+                            if (this.state.currentUser) {
+                                if (match.dater_id !== this.state.currentUser.id) {
+                                    matches.push(match.dater)
+                                } else if (match.matched_with_id !== this.state.currentUser.id) {
+                                    matches.push(match.matched_with)
+                                }
+                                return matches
+                            }
+                        })
+                        this.setState({ matches: matches })
+                    })
+            } 
+            )
+
+    }
+
+    handleUnmatch = (match) => {
+        let itemToUpdate = {
+            id: match.id,
+            match_status_id: 3,
+            dater_id: match.dater_id,
+            matched_with_id: match.matched_with_id
+        }
+
+        APIManager.update(`matches`, itemToUpdate)
+            .then(() => {
+                APIManager.getAll(`matches?match_status_id=2`)
+                    .then(response => this.setState({ matches: response }))
+            })
     }
 
     render() {
         return (
             <>
-            {this.state.matches.map(match => {
-                return <MyMatchItem match={match} />
-            })}
+                {this.state.matches.map((match, i) => {
+                    return <MyMatchItem key={i} match={match} handleUnmatch={this.handleUnmatch} />
+                })
+                }
             </>
         )
     }

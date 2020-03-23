360571da4c422ad475113e0ec8d854161614ea54
Melody Stern <melliemuse@gmail.com>
2020-03-22 20:01:11 -0500

    built functions for user to write a new message

--- src/components/Messaging/MessageHistorySingleUser.js
+++ src/components/Messaging/MessageHistorySingleUser.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import APIManager from '../helpers/APIManager'
 import MessageEach from './MessageEach'
+import NewMessage from './NewMessage'
 
 export default class MessageHistorySingleUser extends Component {
     state = {
@@ -12,13 +13,20 @@ export default class MessageHistorySingleUser extends Component {
         .then(response => this.setState({messages: response}))
     }
 
+    postMessage = (message) => {
+        APIManager.createNew('messages', message)
+            .then(() => APIManager.getAll(`messages?match_id=${this.props.match.params.id}`)
+        .then(response => this.setState({messages: response})))
+    }
+
     render() {
         return (
             <>
             
             {this.state.messages.map(message => {
-                return <MessageEach message={message} key={message.id}/>
+                return <MessageEach message={message} key={message.id} {...this.props}/>
             })}
+            <NewMessage message={this.state.message} postMessage={this.postMessage} {...this.props}/>
             </>
         )
     }

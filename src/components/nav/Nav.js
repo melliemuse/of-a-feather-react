ae9dcd194be1076596136ce7d8b65b3af63c60fe
Melody Stern <melliemuse@gmail.com>
2020-03-25 15:39:20 -0500

    added logour functionality

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { logout, isAuthenticated } from '../helpers/simpleAuth'
 import './Nav.css'
 
-export default class Nav extends Component {
 
-    render() {
+export default function Nav(){
+
+    const history = useHistory()
+
+    const handleLogout = () => {
+        logout()
+        history.push('/')
+    }
+
+   
         return(
             <>
             <ul className="nav">
                 <Link to='/'><li>Home</li></Link>
                 <Link to='/mymatches'><li>My Matches</li></Link>
-                {/* <Link to='/messages'><li>Messages</li></Link> */}
                 <Link to='/userprofile'><li>My Profile</li></Link>
-                <Link to='/register'><li>Register</li></Link>
+                {isAuthenticated ? 
+                <Link to='/' onClick={handleLogout} ><li>Logout</li></Link>
+                :
+                <>
                 <Link to='/login'><li>Login</li></Link>
-                {/* <Link to='/'><li>Logout</li></Link> */}
+                <Link to='/register'><li>Register</li></Link>
+                </>}
             </ul>
             </>
         )
     }
-}
+
